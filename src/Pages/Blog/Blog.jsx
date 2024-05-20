c3bbb3cf78a904ef5594d002c6b314dba8ad94d4
suruzzaman <sajukaligong21@gmail.com>
2024-05-20 22:18:21 +0600

    project all done

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -2,29 +2,33 @@ import Hero from '../../Component/Hero';
 import blog1 from '../../assets/blog-img_01.jpg';
 import blog2 from '../../assets/blog-img_02.jpg';
 import blog3 from '../../assets/blog-img_03.jpg';
+import blog4 from '../../assets/blogpost-img_01.jpg';
+import blog5 from '../../assets/blogpost-img_02.jpg';
+import blog6 from '../../assets/blogpost-img_03.jpg';
 import { FaUser } from 'react-icons/fa';
 import { FaBookOpen } from 'react-icons/fa6';
 import { FaGooglePlusG } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaFacebookF } from 'react-icons/fa';
 import { PiGreaterThan, PiLessThan } from 'react-icons/pi';
+import { FaCaretRight } from 'react-icons/fa';
 
 const Blog = () => {
   return (
     <div className="mb-11">
       <Hero title="Blog"></Hero>
 
-      <div className="px-3 md:px-16 lg:px-44 gap-x-10 flex mt-20">
-        <div className="w-2/3 space-y-8">
+      <div className="px-3 md:px-12 lg:px-44 gap-x-10 flex flex-col md:flex-row mt-10 md:mt-20">
+        <div className="md:w-2/3 space-y-8">
           {/* cards */}
           <div className="">
             <div className="flex relative">
-              <div className=" text-white absolute -left-12 top-[50%] h-full my-auto font-semibold font-serif ">
+              <div className=" text-white absolute md:-left-12 top-[50%] h-full my-auto font-semibold font-serif ">
                 <p className="bg-[#392F2F] px-4 py-2 text-xl">15-08-17</p>
               </div>
               <img className="w-full" src={blog1} alt="" />
             </div>
-            <div className="pl-10 py-3">
+            <div className="md:pl-10 py-3">
               <h1 className="text-xl font-serif font-semibold">
                 Blog Heading here One line
               </h1>
@@ -57,12 +61,12 @@ const Blog = () => {
           </div>
           <div>
             <div className="flex relative">
-              <div className=" text-white absolute -left-12 top-[50%] h-full my-auto font-semibold font-serif ">
+              <div className=" text-white absolute md:-left-12 top-[50%] h-full my-auto font-semibold font-serif ">
                 <p className="bg-[#392F2F] px-4 py-2 text-xl">20-08-17</p>
               </div>
               <img className="w-full" src={blog2} alt="" />
             </div>
-            <div className="pl-10 py-3">
+            <div className="md:pl-10 py-3">
               <h1 className="text-xl font-serif font-semibold">
                 Blog Heading here One line
               </h1>
@@ -95,12 +99,12 @@ const Blog = () => {
           </div>
           <div>
             <div className="flex relative">
-              <div className=" text-white absolute -left-12 top-[50%] h-full my-auto font-semibold font-serif ">
+              <div className=" text-white absolute md:-left-12 top-[50%] h-full my-auto font-semibold font-serif ">
                 <p className="bg-[#392F2F] px-4 py-2 text-xl">22-08-17</p>
               </div>
               <img className="w-full" src={blog3} alt="" />
             </div>
-            <div className="pl-10 py-3">
+            <div className="md:pl-10 py-3">
               <h1 className="text-xl font-serif font-semibold">
                 Blog Heading here One line
               </h1>
@@ -145,10 +149,10 @@ const Blog = () => {
             <button className="border w-9 h-9 flex items-center justify-center rounded-sm hover:bg-[#b8a581] hover:text-white">
               3
             </button>
-            <button className="border w-9 h-9 flex items-center justify-center rounded-sm hover:bg-[#b8a581] hover:text-white">
+            <button className="border w-9 h-9 md:flex items-center justify-center rounded-sm hover:bg-[#b8a581] hover:text-white hidden">
               4
             </button>
-            <button className="border w-9 h-9 flex items-center justify-center rounded-sm hover:bg-[#b8a581] hover:text-white">
+            <button className="border w-9 h-9 md:flex items-center justify-center rounded-sm hover:bg-[#b8a581] hover:text-white hidden ">
               5
             </button>
             <button className="border w-9 h-9 flex items-center justify-center rounded-e-md text-white bg-[#cbb58b] hover:bg-[#b8a581]">
@@ -156,7 +160,102 @@ const Blog = () => {
             </button>
           </div>
         </div>
-        <div className="w-1/3"></div>
+        <div className="md:w-1/3 mt-10 md:mt-0">
+          {/* search */}
+          <div className="flex flex-wrap items-center gap-3 w-full">
+            <div>
+              <input
+                className="border focus:outline-none px-3 py-2 border-gray-300 rounded-sm shadow-lg"
+                type="text"
+                name=""
+                id=""
+                placeholder="Search"
+              />
+            </div>
+            <button className="bg-[#DBC584] px-3 py-2 uppercase font-serif font-semibold text-white border rounded-sm shadow-lg w-full md:w-auto">
+              Search
+            </button>
+          </div>
+          {/* category */}
+          <div className="mt-7">
+            <h1 className="text-2xl font-semibold font-serif mb-4">Category</h1>
+            <div className="flex justify-between items-center border-b pb-3 border-gray-400">
+              <a href="#" className=" font-serif text-gray-600">
+                Vivamus elementum elit
+              </a>
+              <FaCaretRight></FaCaretRight>
+            </div>
+            <div className="flex justify-between items-center border-b pb-3 border-gray-400 mt-3">
+              <a href="#" className=" font-serif text-gray-600">
+                Maecenas ut dui at lorem
+              </a>
+              <FaCaretRight></FaCaretRight>
+            </div>
+            <div className="flex justify-between items-center border-b pb-3 border-gray-400 mt-3">
+              <a href="#" className=" font-serif text-gray-600">
+                Quisque quis libero quis augue
+              </a>
+              <FaCaretRight></FaCaretRight>
+            </div>
+            <div className="flex justify-between items-center border-b pb-3 border-gray-400 mt-3">
+              <a href="#" className=" font-serif text-gray-600">
+                Curabitur consequat odio
+              </a>
+              <FaCaretRight></FaCaretRight>
+            </div>
+            <div className="flex justify-between items-center border-b pb-3 border-gray-400 mt-3">
+              <a href="#" className=" font-serif text-gray-600">
+                Aliquam tristique turpis
+              </a>
+              <FaCaretRight></FaCaretRight>
+            </div>
+          </div>
+          {/* featured */}
+          <div className="mt-7">
+            <h1 className="text-2xl font-semibold font-serif mb-4">
+              Featured Post
+            </h1>
+            <div className="flex gap-x-6 ">
+              <img src={blog4} alt="" />
+              <div>
+                <p className="text-xl font-serif font-semibold">Heading</p>
+                <p className="text-lg text-gray-500">
+                  Lorem Ipsum is simply dummy text of the printing and typeset
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-x-6 border-y border-gray-300 my-4 py-4">
+              <img src={blog5} alt="" />
+              <div>
+                <p className="text-xl font-serif font-semibold">Heading</p>
+                <p className="text-lg text-gray-500">
+                  Lorem Ipsum is simply dummy text of the printing and typeset
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-x-6 ">
+              <img src={blog6} alt="" />
+              <div>
+                <p className="text-xl font-serif font-semibold">Heading</p>
+                <p className="text-lg text-gray-500">
+                  Lorem Ipsum is simply dummy text of the printing and typeset
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* tags */}
+          <div className="mt-7">
+            <h1 className="text-2xl font-semibold font-serif mb-4">Tags</h1>
+            <div className="grid grid-cols-3 gap-4">
+              <button className="border py-1 border-gray-400">Education</button>
+              <button className="border py-1 border-gray-400">Classes</button>
+              <button className="border py-1 border-gray-400">Seat</button>
+              <button className="border py-1 border-gray-400">Teachers</button>
+              <button className="border py-1 border-gray-400">Library</button>
+              <button className="border py-1 border-gray-400">Food</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
